225a7288100971aec198b77e92b68e194179c55c
Jal7823 <jal7823@gmail.com>
2022-03-23 21:12:57 -0300

    ok:working upload images

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,36 @@
 const express = require("express");
 const router = express.Router();
-const policies = require("../../models/polices")
+const policies = require("../../models/polices");
 
 router.get("/", async (req, res) => {
   const policiesDB = await policies.find();
-  res.render('index',{
-    title: 'Index',
-    policies:policiesDB,
+  res.render("index", {
+    title: "Index",
+    policies: policiesDB,
   });
 });
 
 router.get("/base", (req, res) => {
-  res.render('base',{title: 'Base'});
+  res.render("base", { title: "Base" });
 });
 
-router.get('/:id', async (req, res) => {
+router.get("/detail/:id", async (req, res) => {
   const policiesDB = await policies.find();
 
-  res.render('detailsPolice',{
-    title:'Detalle de poliza',
-    policies:policiesDB,
+  res.render("detailsPolice", {
+    title: "Detalle de poliza",
+    policies: policiesDB,
+  });
+});
 
-  })
-})
+router.get("/formPolice", (req, res) => {
+  res.render("formPolices", { title: "Formulario de poliza" });
+});
 
-
+router.post("/formPolice", (req, res) => {
+  
+  console.log('==>',req.file.path);
+  res.send("uploaded");
+});
 
 module.exports = router;
